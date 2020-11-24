ea8e8ec59c47688bdb7b4a07aec5101b3fe69a31
Tobias Büschel <tobias.buschel.15@alumni.ucl.ac.uk>
2020-11-25 03:52:53 +0800

    docs: add refactoring techniques and technical documentation chapters

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,19 @@
 module.exports = {
   sidebar: {
-    'Getting Started': ['introduction', 'contributors'],
-    Handbook: ['code-reviews', 'pair-programming', 'technical-debt', 'debugging-techniques', 'feature-flags', 'branch-based-testing', 'branching-strategies'],
+    'Getting Started': [
+      'introduction',
+      'contributors'
+    ],
+    Handbook: [
+      'code-reviews',
+      'pair-programming',
+      'technical-debt',
+      'feature-flags',
+      'debugging-techniques',
+      'refactoring-techniques',
+      'branch-based-testing',
+      'branching-strategies',
+      'technical-documentation'
+    ],
   },
 };
